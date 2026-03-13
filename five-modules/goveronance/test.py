b73738943aefdb3c3f45de75da47f378b49bf3fc
Shiyuan Liang <shiyuanliang1357@gmail.com>
2026-03-13 14:30:50 -0400

    Create test.py

---
+++ five-modules/goveronance/test.py
@@ -0,0 +1,10 @@
+from five-modules.governance.governance_module import GovernanceModule
+from five-modules.governance.document_loader import load_document
+
+doc = load_document("sample_ai_policy.pdf")
+
+module = GovernanceModule()
+
+result = module.run(doc)
+
+print(result.json(indent=2))
