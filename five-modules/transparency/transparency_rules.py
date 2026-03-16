e6855a29c06a85c79758746c5902162e36a3bf2e
Shiyuan Liang <shiyuanliang1357@gmail.com>
2026-03-16 14:22:40 -0400

    Create transparency_rules.py

---
+++ five-modules/transparency/transparency_rules.py
@@ -0,0 +1,8 @@
+TRANSPARENCY_CRITERIA = [
+    "Model interpretability methods implemented",
+    "Explainability tools such as SHAP or LIME used for model interpretation",
+    "Clear documentation of model design and assumptions",
+    "Model decision-making process is explainable to stakeholders",
+    "Transparency provided regarding model limitations and risks",
+    "Model outputs and explanations accessible to auditors or reviewers"
+]
